602048a16c66a3a275bf09a7ee139f5c95ed50c2
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-05-17 21:01:06 +0100

    Up/Down movement

--- Assets/Scripts/Camera/CameraMover.cs
+++ Assets/Scripts/Camera/CameraMover.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class CameraMover : MonoBehaviour 
 {
-	public float zoomSpeed = 10f;
 	public bool allowThroughOrigin = false;
 
+	public float zoomSpeed = 10f;
 	private float rotateSpeedDegrees = 10f;
 	private float moveSpeedDegrees = 10f;
 
@@ -27,9 +27,14 @@ public class CameraMover : MonoBehaviour
 	private float angleRotatedSoFar_ = 0f;
 	private float destRotationDegrees_ = 0f;
 	
+	private float angleMovedSoFar_ = 0f;
+	private float destMoveDegrees_ = 0f;
+
+	private Camera camera_;
 
 	void Awake()
 	{
+		camera_ = GetComponent< Camera > ( );
 	}
 
 	// Use this for initialization
@@ -37,23 +42,65 @@ public class CameraMover : MonoBehaviour
 	{
 		maxDistFromOrigin_ = AppManager.Instance.maxDist - AppManager.Instance.camerabuffer;
 		minDistFromOrigin_ = AppManager.Instance.camerabuffer;
-		destZoomDistance_  = 0f;
-		distZoomedSoFar_ = 0f;
-	 	destRotationDegrees_ = 0f;
-		angleRotatedSoFar_ = 0f;
+		Stop ( );
 	}
 
 	public void Stop()
 	{
 		destZoomDistance_  = 0f;
+		distZoomedSoFar_ = 0f;
 		destRotationDegrees_ = 0f;
+		angleRotatedSoFar_ = 0f;
+		destMoveDegrees_ = 0f;
+		angleMovedSoFar_ = 0f;		
 		isZooming_ = false;
 		isRotating_ = false;
+		isMoving_ = false;
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
+		if ( isMoving_ )
+		{
+			bool bHasMoved = false;
+			if (Mathf.Abs(destMoveDegrees_) > 0f && Mathf.Abs(angleMovedSoFar_) < Mathf.Abs (destMoveDegrees_))
+			{
+				float sign = (destMoveDegrees_ > 0f)?(1f):(-1f);
+				float angleToMove = sign * moveSpeedDegrees * Time.deltaTime;
+				
+				if ( Mathf.Abs(angleMovedSoFar_ + angleToMove) > Mathf.Abs (destMoveDegrees_))
+				{
+					angleToMove = destMoveDegrees_ - angleMovedSoFar_;
+					angleMovedSoFar_ = 0f;
+					destMoveDegrees_ = 0f;
+				}
+				else
+				{
+					angleMovedSoFar_ += angleToMove;
+				}
+
+				Vector3 v0 = camera_.ScreenToWorldPoint( new Vector3( 0f, -100f, camera_.nearClipPlane ) );
+				Vector3 v1 = camera_.ScreenToWorldPoint( new Vector3( 0f, 100f, camera_.nearClipPlane ) );
+				Vector3 line1 = v1-v0; // horiz line in screen
+
+				Debug.Log ("line is "+line1);
+
+				Vector3 axis = Vector3.Cross(line1,transform.position);
+				Vector3 point = Vector3.zero;
+
+				Debug.Log ( "Move "+(sign*angleToMove)+" degrees  about pt "+point+" axis"+axis);
+				transform.RotateAround( point, axis, -1f * angleToMove);
+				bHasMoved = true;
+			}
+			if (!bHasMoved)
+			{
+				isMoving_ = false;
+				Debug.Log("Camera move finished");
+			}
+
+		}
+
 		if (isZooming_) 
 		{
 			bool bHasZoomed = false;
@@ -128,6 +175,16 @@ public class CameraMover : MonoBehaviour
 
 	void MoveUpOrDown(bool up)
 	{
+		angleMovedSoFar_ = 0f;
+
+		float oldDest = destMoveDegrees_;
+		destMoveDegrees_ = moveStepDegrees;
+		if ( !up )
+		{
+			destMoveDegrees_ *= -1f;
+		}
+		Debug.Log("Rotate "+( (up)?("Up"):("Down")) +" changed destRotation from" + oldDest+" to " +destMoveDegrees_ );
+		isMoving_ = true;
 	}
 
 	void RotateLeftOrRight(bool left)
