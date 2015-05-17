c937308823965c7897f7cf921a12593be300939f
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-05-17 21:40:26 +0100

    Wall, including old shaders and new inside out shader

---
+++ Assets/Scripts/World/Wall.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour 
+{
+
+	void Awake()
+	{
+		/*
+		MeshFilter meshFilter = GetComponent< MeshFilter > ( );
+
+		int[] triangles = meshFilter.sharedMesh.triangles;
+
+		for ( int i = 0; i < triangles.Length; i += 3 )
+		{
+			int tmp = triangles[i];
+			triangles[i] = triangles[ i+1 ];
+			triangles[i] = tmp;
+		}
+
+		Mesh mesh = new Mesh();
+		mesh.vertices = meshFilter.sharedMesh.vertices;
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals();
+		mesh.RecalculateBounds();
+		mesh.Optimize();
+
+		meshFilter.sharedMesh = mesh;
+		*/
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
