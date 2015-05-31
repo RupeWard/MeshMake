1debc1c1415161650b92b9ae1f73cd9c28359cac
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-05-31 21:19:12 +0100

    WIP

---
+++ Assets/Scripts/Mesh/MG/UVProvider/NewGridUVProvider.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MG.UV
+{
+	public class NewGridUVProvider : I_RectUVProvider
+	{
+		private ElementStates.EState state_ = ElementStates.EState.NONE;
+		private GridPosition position_;
+		private float bottom = 0f;
+		private float top = 0f;
+		private float left = 0f;
+		private float right  = 0f;
+		private Vector2 [][] baseUVset_ = null;
+		
+		protected Vector2 [] baseUVs_ = null;
+		
+		private int numRows_ = 0;
+		private int numColumns_ = 0;
+		
+		UV.GridPosition defaultRectGridPosition_;
+
+
+		public void SetState(ElementStates.EState state)
+		{
+			if ( state_ != state )
+			{
+				ElementStates.EState oldState = state_;
+				state_ = state;
+
+				/*
+				GridPosition gp = GetPositionForState(state_);
+				SetPosition(gp);
+				Debug.LogWarning ("State changed from "+oldState+" to "+state_+", pos = "+gp.DebugDescribe());
+				*/
+				SetPosition(GetPositionForState(state_));
+			}
+		}
+
+		public void AddPositionForState(ElementStates.EState state, UV.GridPosition pos)
+		{
+			if ( positionsByState.ContainsKey ( state ) )
+			{
+				positionsByState[state] = pos;
+			}
+			else
+			{
+				positionsByState.Add( state, pos);
+			}
+		}
+
+		private Dictionary< ElementStates.EState, UV.GridPosition> positionsByState = new Dictionary<ElementStates.EState, GridPosition> ( )
+		{
+		};
+
+		public UV.GridPosition GetPositionForState(ElementStates.EState state)
+		{
+			UV.GridPosition result = defaultRectGridPosition_;
+			if ( positionsByState.ContainsKey ( state ) )
+			{
+				result = positionsByState [ state ];
+//				Debug.LogWarning("Found position "+result.DebugDescribe()+"for state "+state);
+			}
+			else
+			{
+				Debug.LogError("No position for state "+state);
+			}
+			return result;
+		}
+
+		public NewGridUVProvider(int c, int r, GridPosition d)
+		{
+			defaultRectGridPosition_ = d;
+			position_ = defaultRectGridPosition_;
+			numRows_ = r;
+			numColumns_ = c;
+			baseUVset_ = new Vector2[][]
+			{
+				new Vector2[]{
+					new Vector2 ( 0f, 0f ),
+					new Vector2 ( 0f, 1f ),
+					new Vector2 ( 1f, 1f )
+				},
+				new Vector2[]{
+					new Vector2 ( 1f, 1f ),
+					new Vector2 ( 1f, 0f ),
+					new Vector2 ( 0f, 0f )
+				},
+			};
+		}
+
+		private NewGridUVProvider()
+		{
+		}
+
+		int currentTriangleNumber_ = -1;
+
+		private Vector2 GetUVForTriangleIndex ( int triangleNumber, int vertexNumber )
+		{
+			if (triangleNumber != currentTriangleNumber_)
+			{
+				currentTriangleNumber_ = triangleNumber;
+				baseUVs_ = baseUVset_[triangleNumber];// baseUVset_[triangleNumber];_
+//				Debug.Log ("UVs changed for triangle "+triangleNumber+" = "+uvs.Length);
+			}
+			Vector2 baseV = baseUVs_[vertexNumber];
+
+			baseV.x = left + baseV.x * (right -left);
+			baseV.y = bottom + baseV.y * (top -bottom);
+
+//			Debug.LogWarning ("UV "+vertexNumber+" (tri "+triangleNumber+") for "+state_+" "+position_.DebugDescribe()+" = "+baseV);
+			return baseV;
+		}
+
+		public void GetUVsForState( int triangleNumber, ElementStates.EState state, ref Vector2[] uvsOut)
+		{
+			for(int i=0; i<3; i++)
+			{
+				uvsOut[i] = GetUVForState(triangleNumber, i, state); 
+			}
+		}
+
+		public Vector2 GetUVForState (int triangleNumber, int vertexNumber, ElementStates.EState state )
+		{
+			Vector2 uv = Vector2.zero;
+			SetState(state);
+			uv = GetUVForTriangleIndex ( triangleNumber, vertexNumber );
+			return uv;
+		}
+
+		private void SetPosition(GridPosition pos)
+		{
+
+			bottom = (float)pos.row/(float) numRows_;
+			top = (float)( pos.row + 1 ) / (float)numRows_;
+			left = (float)pos.column / (float)numColumns_;
+			right = (float)( pos.column + 1 ) / (float)numColumns_;
+
+			if ( float.IsNaN ( bottom ) || float.IsNaN ( top ) || float.IsNaN ( left ) || float.IsNaN ( right ) )
+			{
+				Debug.LogError("NAN!!"
+				               +"\nnumRows = "+numRows_
+				               +"\nnumColumns = "+numColumns_
+				               );
+			}
+		}
+
+	}
+}
--- Assets/Scripts/Mesh/MG/UVProvider/SimpleGridTextureUVProvider.cs
+++ Assets/Scripts/Mesh/MG/UVProvider/SimpleGridTextureUVProvider.cs
@@ -1,39 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//  NOT YET USED
-public class SimpleGridTextureUVProvider : MonoBehaviour 
+namespace MG.UV
 {
-	public Texture2D texture;
-	public int numColumns;
-	public int numRows;
-	public string[] subTextureNames;
 
-	private MG.UV.GridPosition[] gridPositions_;
-
-	void Awake()
+	public class SimpleGridTextureUVProvider : MonoBehaviour 
 	{
-		if ( subTextureNames.Length != numColumns * numRows )
+		static public readonly UV.GridPosition cyanRectGridPosition = new UV.GridPosition ( 0,0 );
+		static public readonly UV.GridPosition greyRectGridPosition = new UV.GridPosition( 1,0);// grey in color3x3
+		static public readonly UV.GridPosition greenRectGridPosition = new UV.GridPosition( 2,0);// green in color3x3
+		
+		static public readonly UV.GridPosition mauveRectGridPosition = new UV.GridPosition ( 0,1 );
+		static public readonly UV.GridPosition purpleRectGridPosition = new UV.GridPosition( 1,1);// purpkke in color3x3
+		static public readonly UV.GridPosition blackRectGridPosition = new UV.GridPosition( 2,1);// black in color3x3
+		
+		static public readonly UV.GridPosition yellowRectGridPosition = new UV.GridPosition ( 0,2 );
+		static public readonly UV.GridPosition redRectGridPosition = new UV.GridPosition( 1,2);// purpkke in color3x3
+		static public readonly UV.GridPosition blueRectGridPosition = new UV.GridPosition( 2,2);// blue in color3x3
+		
+
+		public Material material;
+		public int numColumns;
+		public int numRows;
+		public string[] subTextureNames;
+
+		private MG.UV.GridPosition[] gridPositions_;
+
+		private NewGridUVProvider gridUvProvider_ = null;
+
+		[System.Serializable]
+		public class StatePosition
 		{
-			Debug.LogError("UVProvider "+gameObject.name+" has wrong number of names"); 
+			public ElementStates.EState state = ElementStates.EState.NONE;
+			public int column = 0;
+			public int row = 0;
 		}
-		gridPositions_ = new MG.UV.GridPosition[ numColumns * numRows];
-		for ( int r=0; r<numRows; r++ )
+
+		public StatePosition[] statePositions = new StatePosition[0];
+
+		void Awake()
 		{
-			for ( int c=0; c<numColumns; c++ )
+			if ( subTextureNames.Length != numColumns * numRows )
 			{
-				gridPositions_[ c+r*numColumns] = new MG.UV.GridPosition(c,r);
+				Debug.LogError("UVProvider "+gameObject.name+" has wrong number of names"); 
 			}
-		}
-	}
+			/*
+			gridPositions_ = new MG.UV.GridPosition[ numColumns * numRows];
+			for ( int r=0; r<numRows; r++ )
+			{
+				for ( int c=0; c<numColumns; c++ )
+				{
+					gridPositions_[ c+r*numColumns] = new MG.UV.GridPosition(c,r);
+				}
+			}*/
+			gridUvProvider_ = new NewGridUVProvider(numColumns, numRows, new GridPosition(0,0));
+			foreach ( StatePosition statePos in statePositions )
+			{
+				gridUvProvider_.AddPositionForState( statePos.state, new GridPosition(statePos.column, statePos.row));
+			}
+/*
+			gridUvProvider_.AddPositionForState(ElementStates.EState.Original, cyanRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.GrowingRand, redRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.GrowingClicked, greenRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.GrowingBall, mauveRectGridPosition);
+			
+			gridUvProvider_.AddPositionForState(ElementStates.EState.CollapsingRand, yellowRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.CollapsingClicked, yellowRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.CollapsingBall, yellowRectGridPosition);
+			
+			gridUvProvider_.AddPositionForState(ElementStates.EState.StaticRand, blackRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.StaticClicked, blueRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.StaticBall, purpleRectGridPosition);
+			
+			gridUvProvider_.AddPositionForState(ElementStates.EState.Rand, blackRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.Clicked, blackRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.Ball, blackRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.Collapsing, blackRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.Growing, blackRectGridPosition);
+			gridUvProvider_.AddPositionForState(ElementStates.EState.Static, blackRectGridPosition);
 
-	void Start () 
-	{
+*/
+		}
+
+		void Start () 
+		{
 	
-	}
+		}
 	
-	void Update () 
-	{
+		void Update () 
+		{
 	
+		}
 	}
 }
